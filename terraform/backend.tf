a3a983bacac0c4eeb71696c64fc32d7f49c24433
urwashi <ulavsare>
2024-03-04 08:48:16 +0000

    modifief backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,11 @@
 terraform {
+  required_version = "~> 0.12.0"
   backend "remote" {
-    organization = "web"
-
+    organization = "my-org"
+    hostname = "app.terraform.io"
     workspaces {
-      name = "dev-prod"
+      prefix = "my-org-"
     }
+    token = "Wq73jtM5at6PEg.atlasv1.21GLHd1bk0Gt7TMuAI6Kj70mEw7CvB7wL6Re2nljIyycm6hiIuyOCAB87iMxEDyFeYk"
   }
 }
-
