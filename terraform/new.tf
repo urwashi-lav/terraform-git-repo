756d338b0f8f423f20139295df73a93fbeabae18
urwashi-lav <147142939+urwashi-lav@users.noreply.github.com>
2024-03-25 16:01:38 +0530

    Update new.tf

--- terraform/new.tf
+++ terraform/new.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "dev" {
   ami           = var.aws_ami
   instance_type = var.instance_type
-  provider = "aws.ap-south-1"
+  provider = ["aws.ap-south-1"]
   tags = {
     Name = "dev"
   }
