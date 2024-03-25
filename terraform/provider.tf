a59aa01df842df17d5c67b275b9ccab2ddef1142
urwashi-lav <147142939+urwashi-lav@users.noreply.github.com>
2024-03-25 16:13:07 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
   alias = "us-east-1"
   region = "us-east-1"
